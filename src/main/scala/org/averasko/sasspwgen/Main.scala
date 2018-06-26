bcb7b09949024d024307ef4fcf948e54f8f834cf
Aleh Veraskouski <aleh.veraskouski@gmail.com>
2018-06-26 18:20:53 -0400

    fixed the problem with too high mem usage; who would imagine that scala is so sensitive to random unused variables

--- src/main/scala/org/averasko/sasspwgen/Main.scala
+++ src/main/scala/org/averasko/sasspwgen/Main.scala
@@ -1,9 +1,24 @@
 package org.averasko.sasspwgen
 
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
 object Main extends App {
 
   override def main(args: Array[String]): Unit = {
 
+    timedFuture(computeFuture)
+
+    println("computing...")
+    readLine()
+  }
+
+  def computeFuture = Future{
+    compute()
+    "Foo"
+  }
+
+  def compute(): Unit = {
     val s1 = "Hair"
     val s2 = "Of"
     val s3 = "Glory"
@@ -14,17 +29,28 @@ object Main extends App {
     val w4 = Generators.numbers(2).map(String.valueOf)
     val w5 = Generators.symbols
 
-    val w12 = w1.flatMap(l => Combinators.insertRnd(l, w2))
-
-    val w123 = w12.flatMap(l => Combinators.insertRnd(l, w3))
-
-    val w1234 = w123.flatMap(l => Combinators.insertRnd(l, w4))
-
-    val w12345 = w1234.flatMap(l => Combinators.insertRnd(l, w5)).map(Transforms.merge)
-
-    w12345.map(println).map(s => 1).fold(0)((x1, x2) => x1 + x2)
-    //println(s"with length of ${w1234.length}")
-
+    val elemCount = w1
+      .flatMap(l => Combinators.insertRnd(l, w2))
+      .flatMap(l => Combinators.insertRnd(l, w3))
+      .flatMap(l => Combinators.insertRnd(l, w4))
+      .flatMap(l => Combinators.insertRnd(l, w5))
+      .map(Transforms.merge)
+      .map(println)
+      .map(s => 1)
+      .foldLeft(0)((x1, x2) => x1 + x2)
   }
+
+
+
+
+  def timedFuture[T](future: Future[T]) = {
+    val start = System.currentTimeMillis()
+    future.onComplete({
+      case _ => println(s"Done!\nFuture took ${System.currentTimeMillis() - start} ms")
+    })
+    future
+  }
+
+
 
 }
