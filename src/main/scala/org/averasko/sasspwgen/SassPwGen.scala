c5c145d53ee748895a4df74e82aa2cde4da881f9
Aleh Veraskouski <aleh.veraskouski@gmail.com>
2018-06-27 16:22:21 -0400

    abstracting out the strategy logic a bit; more work tbd

--- src/main/scala/org/averasko/sasspwgen/SassPwGen.scala
+++ src/main/scala/org/averasko/sasspwgen/SassPwGen.scala
@@ -37,25 +37,8 @@ object SassPwGen extends App {
   }
 
   def computeStream(): Seq[String] = {
-    val s1 = "Hair"
-    val s2 = "Of"
-    val s3 = "Glory"
-
-    val w1 = Transforms.capitalizeEasy(s1).map(s => List(s))
-    val w2 = Transforms.capitalizeEasy(s2)
-    val w3 = Transforms.capitalizeEasy(s3)
-    val w4 = Generators.numbers(1).map(String.valueOf)
-    val w5 = Generators.symbols
-
-    w1.flatMap(l => Combinators.insertRnd(l, w2))
-      .flatMap(l => Combinators.insertRnd(l, w3))
-      .flatMap(l => Combinators.insertRnd(l, w4))
-      .flatMap(l => Combinators.insertRnd(l, w5))
-      .map(Transforms.merge)
+    new org.averasko.sasspwgen.HairOfGloryStrategy().compute()
   }
-
-
-
 
   def timedFuture[T](future: Future[T]) = {
     val start = System.currentTimeMillis()
--- src/main/scala/org/averasko/sasspwgen/Strategies.scala
+++ src/main/scala/org/averasko/sasspwgen/Strategies.scala
@@ -1,7 +1,35 @@
 package org.averasko.sasspwgen
 
+class Strategy {
+
+  def compute() : Seq[String] = ???
+
+}
+
+
 object Strategies {
 
 
+
+}
+
+class HairOfGloryStrategy extends Strategy {
+  override def compute (): Seq[String] = {
+    val s1 = "Hair"
+    val s2 = "Of"
+    val s3 = "Glory"
+
+    val w1 = Transforms.capitalizeEasy (s1).map (s => List (s) )
+    val w2 = Transforms.capitalizeEasy (s2)
+    val w3 = Transforms.capitalizeEasy (s3)
+    val w4 = Generators.numbers (1).map (String.valueOf)
+    val w5 = Generators.symbols
+
+    w1.flatMap (l => Combinators.insertRnd (l, w2) )
+    .flatMap (l => Combinators.insertRnd (l, w3) )
+    .flatMap (l => Combinators.insertRnd (l, w4) )
+    .flatMap (l => Combinators.insertRnd (l, w5) )
+    .map (Transforms.merge)
+  }
 }
 
